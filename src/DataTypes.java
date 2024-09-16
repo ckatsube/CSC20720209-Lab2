c958a8232491570174498c3fc478510aef038c95
KarloSins <92882549+KarloSins@users.noreply.github.com>
2024-09-16 15:54:07 -0400

    message

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -3,10 +3,11 @@ import java.util.List;
 public class DataTypes {
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
+
         }
         return s;
     }
